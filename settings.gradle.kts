1a1f40b3bfa3c443f214046f8e32bb0d0036d77f
maksymstanislavenko <maksprogrammer@gmail.com>
2024-03-30 02:17:43 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "TestWeb"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
